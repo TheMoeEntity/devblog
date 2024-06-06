ff7d67aca33e0757a403dd85f933446b4c28e4ba
Anthony <crayonno.o@gmail.com>
2024-06-06 09:14:58 +0100

    framer motion

--- src/app/post/components/Comment.tsx
+++ src/app/post/components/Comment.tsx
@@ -2,37 +2,59 @@ import { type Comments } from "@/components/types";
 import Image from "next/image";
 import React, { type ReactElement } from "react";
 import { Clock } from "./svg";
+import { motion } from "framer-motion";
 
 const CommentComp = ({ comment }: { comment: Comments }): ReactElement => {
   return (
-    <div className="space-y-3 rounded-lg border-[2px] border-[#F9F9FA] p-4 lg:p-6 bg-lightTwo dark:bg-darkTwo ">
-      <header className="flex w-full space-x-3 items-center ">
-        <div className="h-[50px] w-[50px] relative rounded-full overflow-hidden dark:text-white ">
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      whileInView={{ opacity: 1, x: 0,  }}
+      className="space-y-3 rounded-lg  border-[2px] border-[#F9F9FA] p-4 lg:p-6 bg-lightTwo dark:bg-darkTwo "
+    >
+      <header className="flex w-full space-x-3 items-center">
+        <motion.div
+          className="h-[50px] w-[50px] relative rounded-full overflow-hidden dark:text-white"
+          initial={{ scale: 1.5 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <Image
             layout="fill"
-            className="transition transform hover:scale-150 duration-300"
+            className="transition-transform duration-300"
             objectFit="cover"
             alt="img"
             src={"/moonlight.jpg"}
           />
-        </div>
-        <div className=" flex flex-col justify-center text-start dark:text-white text-black ">
-          <span className="text-[18px] capitalize   font-[500] ">
+        </motion.div>
+        <div className="flex flex-col justify-center text-start dark:text-white text-black">
+          <motion.span
+            className="text-[18px] capitalize font-[500]"
+            initial={{ x: 50 }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 0.5, delay: 0.25 }}
+          >
             {comment.username}
-          </span>
-          <p className="flex font-[400]  space-x-2 text-[11px] ">
+          </motion.span>
+          <motion.p
+            className="flex font-[400] space-x-2 text-[11px]"
+            initial={{ y: 50 }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             <span>{comment.date}</span> <Clock />
-          </p>
+          </motion.p>
         </div>
       </header>
 
-      <p className="w-full text-[14px] dark:text-[#d1d1d1] text-black font-[400] text-start ">
-        {/* {comment.comment} */}
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam ad
-        esse natus harum id architecto nisi doloremque dolor exercitationem
-        dolorem.
-      </p>
-    </div>
+      <motion.p
+        className="w-full text-[14px] dark:text-[#d1d1d1] text-black font-[400] text-start overflow-hidden"
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1  }}
+      >
+        {comment.comment}
+      </motion.p>
+    </motion.div>
   );
 };
 
